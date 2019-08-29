fb2bb4c07b1272a87eb4e57279f77275771c14f1
jcamacaro <jesus.alejandr@gmail.com>
2019-08-29 11:02:14 -0430

    agregando scripts

---
+++ app3.js
@@ -0,0 +1,16 @@
+console.log('Inicio del programa');
+
+setTimeout( function() {
+	console.log('Primer time');
+}, 3000 );
+
+setTimeout( function() {
+	console.log('Segundo time');
+}, 0 );
+
+setTimeout( function() {
+	console.log('Tercer time');
+}, 0 );
+
+
+console.log('Fin del programa');
